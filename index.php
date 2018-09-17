43d20c15621b1ba8143bbdfe248187830fea45e7
Administrator <server>
2018-09-17 13:29:07 +0800

    切割公共文件

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="it">
-<head>
-<meta content="text/html; charset=UTF-8" http-equiv="content-type">
-<title>Retwis - Example Twitter clone based on the Redis Key-Value DB</title>
-<link href="css/style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<div id="page">
-<div id="header">
-<a href="/"><img style="border:none" src="logo.png" width="192" height="85" alt="Retwis"></a>
-<div id="navbar">
-<a href="index.php">主页</a>
-| <a href="timeline.php">热点</a>
-
-| <a href="logout.php">退出</a>
-
-</div>
-</div>
+<?php include 'header.php' ?>
 <div id="welcomebox">
 <div id="registerbox">
 <h2>注册!</h2>
@@ -54,7 +36,4 @@
 <li>PHP与redis服务器的连接用pecl的官方扩展<a href="pecl.php.net/package/redis">php-redis</a>
 </ul>
 </div>
-<div id="footer">redis版本的仿微博项目 <a href="http://redis.io">Redis key-value database</a></div>
-</div>
-</body>
-</html>
+<?php include 'footer.php'?>
